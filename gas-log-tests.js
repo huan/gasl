272b88319a431de58b992103db4fb1245ae6f737
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2015-12-10 14:31:03 +0800

    style log demo

--- gas-log-tests.js
+++ gas-log-tests.js
@@ -1,33 +1,64 @@
-var log = new GasLog({
-  printDriver: 'Logger'
-  , level: 'DEBUG'
-})
+'use strict'
+/**
+*
+* GasL - Logger-framework for Google Apps Script
+*
+* Github: https://github.com/zixia/gasl
+*
+* Example:
+    ```
+    var gasLogLib='https://raw.githubusercontent.com/zixia/gasl/master/gas-log.js'
+    var GasLog = eval(UrlFetchApp.fetch(gasLogLib).getContentText())
+  
+    var log = new GasLog({
+      logLevel: 'DEBUG'
+    })
+    
+    log(log.INFO, 'Hello, %s!', 'World')
 
-function testLoop() {
+```
+*/
+
+
+function logLoopAll() {
+  
+  //////////////////////////////////////////////////////////////////////////////////////////
+  ///// GasLog include header start
+  var gasLogLib='https://raw.githubusercontent.com/zixia/gasl/master/gas-log.js'
+  var GasLog = eval(UrlFetchApp.fetch(gasLogLib).getContentText())
+  
+  var log = new GasLog({
+    printDriver: 'Logger'
+    , logLevel: 'INFO'
+  })
+  ///// GasLog include header end
+  //////////////////////////////////////////////////////////////////////////////////////////
+  
   for (var n=0; n<=log.DEBUG; n++) {
-    testLog(n)
+    testLog_(n)
   }
   
   for (var logName in log.LOG_LEVELS) {
-    testLog(logName)
+    testLog_(logName)
+  }
+
+  function testLog_(level) {
+    
+    log.setLogLevel(level)
+    
+    log(log.EMERG, '### Current log level is %s : %s ###', level, log.getLogLevel())
+    log('Hello, I am a default level message')  
+    
+    log(log.EMERG  , 'this is a %s level msg', 'EMERG')
+    log(log.ALERT  , 'this is a %s level msg', 'ALERT')
+    log(log.CRIT   , 'this is a %s level msg', 'CRIT')
+    log(log.ERR    , 'this is a %s level msg', 'ERR')
+    log(log.WARNING, 'this is a %s level msg', 'WARNING')
+    log(log.NOTICE , 'this is a %s level msg', 'NOTICE')
+    log(log.INFO   , 'this is a %s level msg', 'INFO')
+    log(log.DEBUG  , 'this is a %s level msg', 'DEBUG')
+    
   }
   
-}
-function testLog(level) {
- 
-  log.setLogLevel(level)
-  
-  log(log.EMERG, '### Current log level is %s : %s ###', level, log.getLogLevel())
-  log('Hello, I am a default level message')  
-  
-  log(log.EMERG, 'this is a %s level msg', 'EMERG')
-  log(log.ALERT, 'this is a %s level msg', 'ALERT')
-  log(log.CRIT, 'this is a %s level msg', 'CRIT')
-  log(log.ERR, 'this is a %s level msg', 'ERR')
-  log(log.WARNING, 'this is a %s level msg', 'WARNING')
-  log(log.NOTICE, 'this is a %s level msg', 'NOTICE')
-  log(log.INFO, 'this is a %s level msg', 'INFO')
-  log(log.DEBUG, 'this is a %s level msg', 'DEBUG')
-
 }
 
